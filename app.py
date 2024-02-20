54e57689d13215aa79f9fb834add8c9c562fd594
sankalp1806 <132252437+sankalp1806@users.noreply.github.com>
2024-02-20 16:01:26 +0530

    app.py

--- app.py
+++ app.py
@@ -212,8 +212,8 @@ def generate_wordcloud_with_words(text, included_words):
 
 
 # Streamlit App
-st.sidebar.markdown("**Whatsapp Chat Analyzer**<br>")
-st.sidebar.markdown("<sub><b>by SANKALP SHARMA</b></sub>", unsafe_allow_html=True)
+st.sidebar.markdown("**Whatsapp Chat Analyzer**")
+st.sidebar.markdown(""<p style='font-size:small'><b>By SANKALP SHARMA</b></p>", unsafe_allow_html=True)
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
